bda3f4a5807be2fc35fcd1bc140e97e76373cf6c
oleglego94 <olegik94b@gmail.com>
2020-11-14 02:07:26 +0200

    make formater for diff dictionary

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,22 +1,23 @@
 from collections import OrderedDict
-from loader import load_file
+from gendiff.loader import load_file
+from gendiff.stylish import format_dict
 
 
 def build_diff_dict(dict_before, dict_after):
     diff = {}
 
-    dict_before_keys = set(dict_before.keys())
-    dict_after_keys = set(dict_after.keys())
+    before_keys = set(dict_before.keys())
+    after_keys = set(dict_after.keys())
 
-    added_keys = dict_after_keys - dict_before_keys
+    added_keys = after_keys - before_keys
     for k in added_keys:
         diff[k] = ('ADDED', dict_after[k])
 
-    removed_keys = dict_before_keys - dict_after_keys
+    removed_keys = before_keys - after_keys
     for k in removed_keys:
         diff[k] = ('REMOVED', dict_before[k])
 
-    common_keys = dict_before_keys & dict_after_keys
+    common_keys = before_keys & after_keys
     for k in common_keys:
         old_v = dict_before[k]
         new_v = dict_after[k]
@@ -27,10 +28,11 @@ def build_diff_dict(dict_before, dict_after):
         else:
             diff[k] = ('UNCHANGED', old_v)
 
-    return diff
+    return OrderedDict(sorted(diff.items()))
 
 
 def generate_diff(file_path_before, file_path_after):
     before_dict = load_file(file_path_before)
     after_dict = load_file(file_path_after)
-    return build_diff_dict(before_dict, after_dict)
+    diff_dictionary = build_diff_dict(before_dict, after_dict)
+    return format_dict(diff_dictionary)
--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -1,7 +1,41 @@
-def encode(value):
-    if value is False:
-        return 'false'
-    elif value is True:
-        return 'true'
-    else:
-        return value
+def format_dict(diff_dict, lvl=0):
+    result = '{\n'
+    tab = '    ' * lvl
+
+    def encode(value):
+        if isinstance(value, dict):
+            return format_dict(value, lvl+1)
+
+        if value is False:
+            return 'false'
+        elif value is True:
+            return 'true'
+        elif value is None:
+            return 'null'
+        else:
+            return value
+
+    for k, v in diff_dict.items():
+        if isinstance(v, tuple):
+            state, val = v[:2]
+        else:
+            state = None
+            val = v
+
+        if state == 'NESTED':
+            result += (tab + '    {}: {}\n'.format(k, format_dict(val, lvl+1)))
+
+        elif state == 'ADDED':
+            result += (tab + '  + {}: {}\n'.format(k, encode(val)))
+
+        elif state == 'REMOVED':
+            result += (tab + '  - {}: {}\n'.format(k, encode(val)))
+
+        elif state == 'CHANGED':
+            result += (tab + '  - {}: {}\n'.format(k, encode(val)))
+            result += (tab + '  + {}: {}\n'.format(k, encode(v[2])))
+
+        else:
+            result += (tab + '    {}: {}\n'.format(k, encode(val)))
+
+    return ('{}'.format(result) + tab + '}')
